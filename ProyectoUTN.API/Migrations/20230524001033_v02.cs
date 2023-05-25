9f3b70622cd34aadcc83aa10dac9f2a4715e810f
Dylanjesus65 <dylanjesus65@gmail.com>
2023-05-24 21:27:46 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoUTN.API/Migrations/20230524001033_v02.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoUTN.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class v02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Categorias_CategoriaCodigoCategoria",
+                table: "Productos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaCodigoCategoria",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Categorias_CategoriaCodigoCategoria",
+                table: "Productos",
+                column: "CategoriaCodigoCategoria",
+                principalTable: "Categorias",
+                principalColumn: "CodigoCategoria");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Categorias_CategoriaCodigoCategoria",
+                table: "Productos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaCodigoCategoria",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Categorias_CategoriaCodigoCategoria",
+                table: "Productos",
+                column: "CategoriaCodigoCategoria",
+                principalTable: "Categorias",
+                principalColumn: "CodigoCategoria",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ ProyectoUTN.WebMVC/Controllers/ProductosController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoUTN.Modelos;
+using ProyectoUTN.UAPI;
+
+namespace ProyectoUTN.WebMVC.Controllers
+{
+    public class ProductosController : Controller
+    {
+        
+        private string Url= "https://localhost:7241/api/Productos";
+        private Crud<Producto> Crud { get; set; }
+        public ProductosController()
+        {
+            Crud = new Crud<Producto>();
+        }
+
+        // GET: ProductosController
+        public ActionResult Index()
+        {
+            var datos =  Crud.Select(Url);
+            return View(datos);
+        }
+
+        // GET: ProductosController/Details/5
+        public ActionResult Details(int id)
+        {
+            var datos = Crud.Select_ById(Url,id.ToString());
+            return View(datos);
+        }
+
+        // GET: ProductosController/Create
+        public ActionResult Create()
+        {
+            var listaCategorias = new Crud<Categoria>()
+                .Select(Url.Replace("Producto", "Categoria"))
+                .Select(p => new SelectListItem     // transformamos del tipo Privincia -> SelectListItem
+                {
+                    Value = p.CodigoCategoria.ToString(),       // codigo de provincia
+                    Text = p.Nombre               // nombre de provincia
+                })
+                .ToList();
+
+            ViewBag.ListaCategorias = listaCategorias;  // pasamos la lista de Provincias a la vista
+
+            return View();
+        }
+
+        // POST: ProductosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producto datos)
+        {
+            try
+            {
+                 Crud.Insert(Url, datos);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(datos);
+            }
+        }
+
+        // GET: ProductosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var datos = Crud.Select_ById(Url, id.ToString());
+            return View(datos);
+        }
+
+        // POST: ProductosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Producto datos)
+        {
+            try
+            {
+                Crud.Update(Url,id.ToString(), datos);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(datos);
+            }
+        }
+
+        // GET: ProductosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var datos = Crud.Select_ById(Url, id.ToString());
+            return View(datos);
+        }
+
+        // POST: ProductosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Producto datos)
+        {
+            try
+            {
+                Crud.Delete(Url,id.ToString());
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(datos);
+            }
+        }
+    }
+}
